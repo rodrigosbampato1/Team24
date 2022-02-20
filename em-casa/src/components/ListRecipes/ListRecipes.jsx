7ade4e7626be030e14d541fab922bb89d25b61fa
Altieres Schincariol Netto <altieres@upstack.co>
2022-02-20 10:05:07 -0300

    Ajustes nos cards da receita

--- em-casa/src/components/ListRecipes/ListRecipes.jsx
+++ em-casa/src/components/ListRecipes/ListRecipes.jsx
@@ -2,34 +2,82 @@ import React from "react";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import "./ListRecipes.css";
+import Image1 from './img1.jpg'
+import Recipes from '../Recipes/Recipes';
 
 
 export default function ListRecipes() {
-   
-    const footer = (
-        <span>
-            <Button className="botao" label="Selecionar" icon="pi pi-check" />
-            <Button label="Cancelar" icon="pi pi-times" className="p-button-secondary-ml-2" />
-        </span>
-    );
+    const recipes = [
+        {
+            recipeUrl: '#',
+            title: 'Macarrão sem Glúten',
+            image: Image1,
+            content: 'Pensando em facilitar a vida de pessoas celíacas, preparamos um menu com opções livre de glúten.'
+        },
+        {
+            recipeUrl: '#',
+            title: 'Macarrão sem Glúten',
+            image: Image1,
+            content: 'Pensando em facilitar a vida de pessoas celíacas, preparamos um menu com opções livre de glúten.'
+        },
+        {
+            recipeUrl: '#',
+            title: 'Macarrão sem Glúten',
+            image: Image1,
+            content: 'Pensando em facilitar a vida de pessoas celíacas, preparamos um menu com opções livre de glúten.'
+        },
+        {
+            recipeUrl: '#',
+            title: 'Macarrão sem Glúten',
+            image: Image1,
+            content: 'Pensando em facilitar a vida de pessoas celíacas, preparamos um menu com opções livre de glúten.'
+        }
+    ]
+
+    const _onBtnClick = (recipeUrl) => {
+        return (
+            <a href={recipeUrl}></a>
+        )
+    }
+
+    const renderHeader = (image) => {
+        return (
+            <img alt='Card' src={image} width='250' />
+        )
+    }
+
+    const renderCard = (recipeUrl, title, image, content) => {
+        return (
+            <div className='col'>
+                <Card 
+                    title={title}
+                    footer={renderFooter(recipeUrl)}
+                    header={renderHeader(image)}
+                >{content}</Card>
+            </div>
+        )
+    }
+
+    const renderFooter = (recipeUrl) => {
+        return (
+            <span>
+                <Button className="botao" label="Selecionar" icon="pi pi-check" onClick={_onBtnClick(recipeUrl)} />
+                <Button label="Cancelar" icon="pi pi-times" className="p-button-secondary-ml-2 ms-1" />
+            </span>
+        )
+    }
 
     return (
-        <div>
-            <div className="card">
-                    <h5>Basic</h5>
-                    <image src= "" alt="Image" width="250" />
-
-                    <h5>Preview</h5>
-                    <image src={Image1} alt="Image" width="250" preview />
+        <div className='cards-grid m-3'>
+            <div className="container d-flex justify-content-around">
+                <div className="row">
+                    {
+                        recipes.map((recipe) => {
+                            return renderCard(recipe.recipeUrl, recipe.title, recipe.image, recipe.content)
+                        })
+                    }
                 </div>
-            <Card  title="Macarrão sem Glúten" style={{ width: '25rem', marginBottom: '2em'}}>
-                <p className="m-0" style={{lineHeight: '1.5'}}>Pensando em facilitar a vida de pessoas celíacas, preparamos um menu com opções livre de glúten.</p>
-            </Card>
-
-            <Card title="Advanced Card" subTitle="Subtitle" style={{ width: '25em' }} footer={footer} header={header}>
-                <p className="m-0" style={{lineHeight: '1.5'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-                    quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
-            </Card>
+            </div>
         </div>
     )
 
