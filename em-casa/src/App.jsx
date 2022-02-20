4bc30ed370a6637d86320401f2f3b836aa025e0d
Bruna Leardini Machado <bruna.leardininet@gmail.com>
2022-02-20 02:04:28 -0300

    colocar imagem

--- em-casa/src/App.jsx
+++ em-casa/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-
+import ListRecipes from './components/ListRecipes/ListRecipes';
 import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
-      <Landing></Landing>
+      {/* <Landing></Landing> */}
+      <ListRecipes></ListRecipes>
     </div>
   );
 }
